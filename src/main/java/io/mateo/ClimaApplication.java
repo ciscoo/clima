5684b65db5dbfa1e8f1973654319c1ce65fd6136
ciscoo <cisco@mateo.io>
2016-09-29 22:54:31 -0500

    Hello Spring Boot.

---
+++ src/main/java/io/mateo/ClimaApplication.java
@@ -0,0 +1,12 @@
+package io.mateo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimaApplication.class, args);
+	}
+}
